73940e55ba3a492928cb5c783ea8dfa01fac20a0
Nikhil Bharadwaj <nikhilbharadwaj08@gmail.com>
2020-04-21 16:47:13 +0530

    Add files via upload

---
+++ main_sub.py
@@ -0,0 +1,41 @@
+import cv2
+cap = cv2.VideoCapture('video6.mp4') # 0 for webcam
+try:
+    with detection_graph.as_default():
+        with tf.Session() as sess:
+                # Get handles to input and output tensors
+                ops = tf.get_default_graph().get_operations()
+                all_tensor_names = {output.name for op in ops for output in op.outputs}
+                tensor_dict = {}
+                for key in [
+                  'num_detections', 'detection_boxes', 'detection_scores',
+                  'detection_classes', 'detection_masks'
+                ]:
+                    tensor_name = key + ':0'
+                    if tensor_name in all_tensor_names:
+                        tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                      tensor_name)
+
+                while True:
+                    ret, image_np = cap.read()
+                    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                    image_np_expanded = np.expand_dims(image_np, axis=0)
+                    # Actual detection.
+                    output_dict = run_inference_for_single_image(image_np, detection_graph)
+                    # Visualization of the results of a detection.
+                    vis_util.visualize_boxes_and_labels_on_image_array(
+                        image_np,
+                        output_dict['detection_boxes'],
+                        output_dict['detection_classes'],
+                        output_dict['detection_scores'],
+                        category_index,
+                        instance_masks=output_dict.get('detection_masks'),
+                        use_normalized_coordinates=True,
+                        line_thickness=8)
+                    cv2.imshow('object_detection', cv2.resize(image_np, (800, 600)))
+                    if cv2.waitKey(25) & 0xFF == ord('q'):
+                        cap.release()
+                        cv2.destroyAllWindows()
+                        break
+except Exception as e:
+    print(e)
